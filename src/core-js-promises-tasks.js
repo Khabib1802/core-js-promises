6b3ca4b6dd6c960fda6e6f3faac0978d8f2b165c
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-02-02 22:57:27 +0300

    feat: solve all tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -135,8 +135,13 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(async (acc, promise) => {
+    const result = await acc;
+    const num = await promise;
+
+    return result + num;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
